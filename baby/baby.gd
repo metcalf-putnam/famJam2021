6904cd25d28666d6913268ae15d2defbe630f5bd
Max Metcalf-Putnam <max.metcalf.putnam@gmail.com>
2021-02-15 23:52:15 -0600

    better method for clearing previous events

--- baby/baby.gd
+++ baby/baby.gd
@@ -4,8 +4,6 @@ var chosen_food
 var accepted_change := 4.0
 var declined_change := -4.0
 var clue_time_seconds = 2
-var check_int
-
 
 func _ready():
 	rng.randomize()
@@ -20,25 +18,24 @@ func pick_food():
 	
 	var rand_num = rng.randi_range(0, num -1)
 	chosen_food = FoodDic.foods[rand_num]
+	reset_clues()
+	think_about_food()
+
+func reset_clues():
 	$Clue1.texture = null
 	$Clue2.texture = null
 	$Clue3.texture = null
-	think_about_food()
-
+	$Tween.remove_all()
 
 func think_about_food():
-	check_int = rng.randi()
 	var texture = load(chosen_food["sprite_path"])
 	
-	$Tween.interpolate_callback(self, 0, "populate_clue", check_int, 1, texture)
-	$Tween.interpolate_callback(self, 1, "populate_clue", check_int, 2, texture)
-	$Tween.interpolate_callback(self, 2, "populate_clue", check_int, 3, texture)
+	$Tween.interpolate_callback(self, 0, "populate_clue", 1, texture)
+	$Tween.interpolate_callback(self, 1, "populate_clue", 2, texture)
+	$Tween.interpolate_callback(self, 2, "populate_clue", 3, texture)
 	$Tween.start()
 
-func populate_clue(event_check_int, clue_number, texture):
-	if (check_int != event_check_int):
-		return
-	
+func populate_clue(clue_number, texture):
 	get_node("Clue" + str(clue_number)).texture = texture
 
 func _on_food_clicked(food_name):
